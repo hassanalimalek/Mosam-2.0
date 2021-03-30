b54b68b935385cf0410a5e3a392fbffd28b24a36
Hassan Ali <hassanalimalek9@gmail.com>
2021-03-30 14:11:38 +0500

    No Internet Case Fixed

--- src/App.js
+++ src/App.js
@@ -1,51 +1,51 @@
 import './App.css';
-import { useState } from 'react'
-import Header from './components/header'
-import WeatherSearch from './components/weatherSearch'
-import WeatherDetails from './components/weatherDetails'
+import {useState} from 'react';
+import Header from './components/header';
+import WeatherSearch from './components/weatherSearch';
+import WeatherDetails from './components/weatherDetails';
 
-import { css } from "@emotion/core";
-import BarLoader from "react-spinners/BarLoader";
-
-
-
+import {css} from '@emotion/core';
+import BarLoader from 'react-spinners/BarLoader';
 
 function App() {
-  let [weatherDetails,setWeatherDetails] = useState('');
-  let [loading,setLoading] = useState(false);
+  let [weatherDetails, setWeatherDetails] = useState('');
+  let [loading, setLoading] = useState(false);
 
   // Initial Loader
   let [initialloader, setInitialLoader] = useState(true);
-  let color = ("#111");
+  let color = '#111';
   const override = css`
-  display: block;
-  color:black;
-  margin: 0 auto;
-`;
-  setTimeout(()=>{
+    display: block;
+    color: black;
+    margin: 0 auto;
+  `;
+  setTimeout(() => {
     setInitialLoader(false);
-  },3000)
+  }, 3000);
 
-
-  let setWeather = (details)=>{
+  let setWeather = (details) => {
     setWeatherDetails(details);
-  }
+  };
 
-  if(initialloader){
+  if (initialloader) {
     return (
       <div className="loaderContainer">
-         <BarLoader color={color} loading={initialloader} css={override} height={6} width={180} />
+        <BarLoader
+          color={color}
+          loading={initialloader}
+          css={override}
+          height={6}
+          width={180}
+        />
       </div>
-    )
-  }
-  else{
+    );
+  } else {
     return (
       <>
-      <Header/>
-      <WeatherSearch setWeather={setWeather} setLoading={setLoading}/>
-      <WeatherDetails weatherDetails={weatherDetails} loading={loading} />
+        <Header />
+        <WeatherSearch setWeather={setWeather} setLoading={setLoading} />
+        <WeatherDetails weatherDetails={weatherDetails} loading={loading} />
       </>
-    
     );
   }
 }
